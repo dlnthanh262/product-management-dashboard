47d017e39e6f7027468a6e73138faf3489c0c2c3
Thanh Do <thanh@pullable.com>
2025-08-12 16:18:35 +0700

    Authorize role Admin for API calling

--- backend/src/main/java/com/dashboard/controller/BrandController.java
+++ backend/src/main/java/com/dashboard/controller/BrandController.java
@@ -10,11 +10,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/brands")
+@PreAuthorize("hasRole('ADMIN')")
 public class BrandController {
 
     private final BrandService brandService;
